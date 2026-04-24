aca1bbded15341e13b2de7b08e93af630e4dc27a
Yahya-Ahmed-09 <156502077+Yahya-Ahmed-09@users.noreply.github.com>
2026-04-24 17:32:07 +0000

    Post and Put Request Created

--- index.js
+++ index.js
@@ -10,6 +10,86 @@ const app = express();
 app.use(cors());
 app.use(express.json());
 
+
+app.get('/', async(req, res)=>{
+    res.json({message: "API "});
+});
+
+
+// Create Post Request
+app.post('/api/users', async(req, res)=>{
+    try {
+        const {name, email} = req.body;
+
+        if(!name || !email){
+            res.status(400).json({
+                message: "Name and Email are Required"
+            });
+        }
+
+        const result = await pool.query(
+             `INSERT INTO users(name, email) VALUES ('${name}', '${email}')`
+        );
+
+        res.json({
+            message: "User Created Successfully",
+            user: result.rows[0]
+        })
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        });
+    }
+});
+
+
+// Update User
+
+app.put('api/users/:id', async(req, res)=>{
+    try {
+        const { id } = req.params
+        const {name, email} = req.body;
+
+        if(!name || !email){
+            res.status(400).json({
+                message: "Please Enter Somthing fro updating user"
+            });
+        }
+
+        const user = await pool.query(
+            `Select * from users WHERE ID = $1`,
+            [id]
+        )
+
+        
+        if((await user).rows.length === 0){
+            return res.json.status(404).json({
+                message: "User Not Found"
+            });
+        }
+        
+        const currentUser = user.rows[0];
+
+        const updatedName = name || currentUser.name;
+        const updatedEmail = email || currentUser.email;
+
+        const result = await pool.query(
+            `UPDATE user Set name = $1, email = $2 WHERE id = $3`,
+            [updatedName, updatedEmail, id]
+        )
+
+        res.status(200).json({
+            message:"User Updated Successfully",
+            user: result.rows[0]
+        })
+        
+    } catch (error) {
+        res.status(500).json({
+            message: error.message
+        })
+    }
+});
+
 /* -------------------------
    READ ALL USERS
 ------------------------- */
